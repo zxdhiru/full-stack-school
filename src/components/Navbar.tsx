4d15318892a513c19ef78d08c0e9c335cbe84c6b
Dhiru <dhiru@alljobguider.in>
2026-04-02 20:44:40 +0530

    Update Navbar component to display user's first and last name dynamically

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,9 @@ const Navbar = async () => {
           </div>
         </div>
         <div className="flex flex-col">
-          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-xs leading-3 font-medium">
+            {user?.firstName} {user?.lastName}
+          </span>
           <span className="text-[10px] text-gray-500 text-right">
             {user?.publicMetadata?.role as string}
           </span>
